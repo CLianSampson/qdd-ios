f9a434a218a2c0150a1fd31e6527a7724be8e8c8
328714598@qq.com <apple@appledembp.lan>
2017-02-18 00:31:11 +0800

    Initial Commit

---
+++ qdd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qdd
+//
+//  Created by Apple on 17/2/18.
+//  Copyright © 2017年 Samposn Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qdd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qdd
+//
+//  Created by Apple on 17/2/18.
+//  Copyright © 2017年 Samposn Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
